a3ffdb41f6ae2b7ad2687735c814264f6fe29275
JEdLeon <jeduardoleon22@hotmail.com>
2023-05-27 00:50:41 -0600

    Last estable version of keeper-app-v2, fullfilled App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import CreateNote from "./CreateNote";
+import Note from "./Note"
+import Footer from "./Footer";
+
+
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function AddNote(newNote) {
+    setNotes([...notes, newNote]);
+  }
+
+  function DeleteNote(id) {
+    setNotes(notes.filter((note) => {
+      return note.id !== id
+    }));
+  }
+
   return (
     <div>
-      Hello world
+      <Header />
+      <CreateNote key={0} AddNote={AddNote} />
+      {notes.map(note => {
+        return <Note key={note.id} note={note} DeleteNote={DeleteNote} />
+      })}
+      <Footer />
     </div>
   );
 }
